6a558364704ec920cd9d1d74288680282a890bbb
Polrojas <polrojas@gmail.com>
2019-10-06 12:16:14 -0300

    Subida general

---
+++ Arkidia/code.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      categorias: [
+        { id:1, nombre: "Pintura", edad: "Edad" },
+        { id:2, nombre: "Cocina", edad: "Edad" },
+        { id:3, nombre: "Dibujo", edad: "Edad" },
+        { id:4, nombre: "Construcción", edad: "Edad" },
+        { id:5, nombre: "Jardinería", edad: "Edad" },
+        { id:6, nombre: "Danza", edad: "Edad" },
+        { id:7, nombre: "Música", edad: "Edad" },
+        { id:8, nombre: "Marta", edad: "Edad" }
+    ]
+    }
+  })
